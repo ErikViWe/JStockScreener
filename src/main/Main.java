ea1e525d3f3d74428e0e8d5b0c2e2ba940cfda32
ErikViWe <erik.weinstock@googlemail.com>
2017-08-08 20:49:15 +0200

    Added Main class.

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+/**
+ * Main class contains the main method only. This method will run at program start and initialize all important components.
+ * @author Erik Weinstock
+ * @version 1.0
+ *
+ */
+public class Main {
+
+	/**
+	 * Main method which runs at program start.
+	 * The String[] args does not need to be filled. No arguments for the program start needed.
+	 * @param args
+	 * 		String[] which will be empty. Elements in it will be ignored at program start
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
