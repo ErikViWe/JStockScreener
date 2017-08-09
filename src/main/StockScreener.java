e12f9604252cb8675c29fa2ade7a032d55c1e4fb
ErikViWe <erik.weinstock@googlemail.com>
2017-08-09 16:32:26 +0200

    Added JavaDoc and StackPane.

--- src/main/StockScreener.java
+++ src/main/StockScreener.java
@@ -1,6 +1,8 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -13,20 +15,32 @@ public class StockScreener extends Application {
 	
 	/**
 	 * Constructor generates the StockScreener object.
+	 * This constructor is executed at the javafx initialization time.
 	 */
 	public StockScreener() {
 		
 	}
 	
+	/**
+	 * Executes the launch method to run the primaryStage.
+	 */
 	public static void run() {
 		launch();
 	}
 	
+	/**
+	 * Initializes the ui and loads the components.
+	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setMaximized(true);
 		primaryStage.setTitle("StockScreener");
-		primaryStage.show();
+		StackPane root = new StackPane();
+		
+		//TODO: add components to the scene
+		
+	    primaryStage.setScene(new Scene(root));
+	    primaryStage.show();
 		
 	}
 	
