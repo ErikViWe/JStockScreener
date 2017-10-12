adda27789bfc80fbd15d20d889bb7850c9d4c4d6
ErikViWe <erik.weinstock@googlemail.com>
2017-10-12 10:25:02 +0200

    Added a separator to the display.

--- src/guiComponents/Display.java
+++ src/guiComponents/Display.java
@@ -1,15 +1,18 @@
 package guiComponents;
 
+import javafx.geometry.Orientation;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.VBox;
 
 public class Display extends VBox {
 	
 	private LineChart<String, Number> chart;
+	private Separator separator;
 	
 	public Display() {
 		init();
@@ -33,6 +36,11 @@ public class Display extends VBox {
 		
 		chart.getData().add(series);
 		this.getChildren().add(chart);
+		
+		separator = new Separator();
+		separator.setOrientation(Orientation.HORIZONTAL);
+		this.getChildren().add(separator);
+		this.setSpacing(10);
 	}
 
 }
