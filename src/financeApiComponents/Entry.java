b788e6d748912c2d47c4115ed2d9eee8d3260be4
ErikViWe <erik.weinstock@googlemail.com>
2017-10-17 19:31:10 +0200

    Added JavaDoc to entry class.

--- src/financeApiComponents/Entry.java
+++ src/financeApiComponents/Entry.java
@@ -13,6 +13,16 @@ public class Entry {
 	private Long volume;
 	private String symbol;
 	
+	/**
+	 * Constructor generates an instance of a set of quotes.
+	 * @param date
+	 * @param open
+	 * @param high
+	 * @param low
+	 * @param close
+	 * @param volume
+	 * @param symbol
+	 */
 	public Entry(Calendar date, BigDecimal open, BigDecimal high, BigDecimal low, BigDecimal close, Long volume, String symbol) {
 		this.date = date;
 		this.open = open;
@@ -23,14 +33,29 @@ public class Entry {
 		this.symbol = symbol;
 	}
 	
+	/**
+	 * Returns the day of month in a numerical format.
+	 * @return
+	 * 		day of month in numerical format
+	 */
 	public String getDay() {
 		return String.valueOf(date.get(Calendar.DAY_OF_MONTH));
 	}
 	
+	/**
+	 * Returns the month of a year in numerical format.
+	 * @return
+	 * 		month of year in numerical format i.e. 1
+	 */
 	public String getMonth() {
 		return String.valueOf(date.get(Calendar.MONTH) + 1);
 	}
 	
+	/**
+	 * Returns the month of a year in textual format. 
+	 * @return
+	 * 		month of year in textual format i.e. January
+	 */
 	public String getMonthValue() {
 		Integer month = date.get(Calendar.MONTH) + 1;
 		switch (month) {
@@ -49,36 +74,76 @@ public class Entry {
 		}	
 	}
 	
+	/**
+	 * Returns the year of the entry.
+	 * @return
+	 * 		year of entry in numerical format
+	 */
 	public String getYear() {
 		return String.valueOf(date.get(Calendar.YEAR));
 	}
 	
+	/**
+	 * Returns the open price of the entry as BigDecimal.
+	 * @return
+	 * 		open price
+	 */
 	public BigDecimal getOpen() {
 		return open;
 	}
 	
+	/**
+	 * Returns the highest price of the entry as BigDecimal.
+	 * @return
+	 * 		highest price
+	 */
 	public BigDecimal getHigh() {
 		return high;
 	}
 	
+	/**
+	 * Returns the lowest price of the entry as BigDecimal.
+	 * @return
+	 * 		lowest price
+	 */
 	public BigDecimal getLow() {
 		return low;
 	}
 	
+	/**
+	 * Returns close price of the entry as BigDecimal.
+	 * @return
+	 * 		close price
+	 */
 	public BigDecimal getClose() {
 		return close;
 	}
 	
+	/**
+	 * Returns the volume of the Entry.
+	 * @return
+	 * 		volume
+	 */
 	public long getVolume() {
 		return volume;
 	}
 	
+	/**
+	 * Returns the stock symbol. Note, the stock symbol is not the ISIN but the yahoo intern stock id.
+	 * @return
+	 * 		stock symbol
+	 */
 	public String getSymbol() {
 		return symbol;
 	}
 	
+	/**
+	 * Returns entry date in the correct display format.
+	 * @return
+	 * 		entry date
+	 */
 	public String getDate() {
-		return this.getDay() + "," + this.getMonth() + "," + this.getYear();
+		return this.getDay() + "." + this.getMonth() + "." + this.getYear();
 	}
 
 }
