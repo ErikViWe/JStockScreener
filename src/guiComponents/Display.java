91f80ca2283d7e2eed938f3253d7a3f80e42850f
ErikViWe <erik.weinstock@googlemail.com>
2017-10-19 16:29:59 +0200

    Made some Changes in display class for a better formation of data.

--- src/guiComponents/Display.java
+++ src/guiComponents/Display.java
@@ -1,5 +1,6 @@
 package guiComponents;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 import financeApiComponents.Entry;
@@ -16,6 +17,8 @@ public class Display extends VBox {
 	
 	private LineChart<String, Number> chart;
 	private Separator separator;
+	private CategoryAxis xAxis;
+	private NumberAxis yAxis;
 	
 	public Display() {
 		init();
@@ -23,8 +26,8 @@ public class Display extends VBox {
 	
 	private void init() {
 		//defining the axis
-		final CategoryAxis xAxis = new CategoryAxis();
-		final NumberAxis yAxis = new NumberAxis();
+		xAxis = new CategoryAxis();
+		yAxis = new NumberAxis();
 		
 		xAxis.setLabel("Date");
 		yAxis.setLabel("Price");
@@ -42,27 +45,55 @@ public class Display extends VBox {
 		
 		separator = new Separator();
 		separator.setOrientation(Orientation.HORIZONTAL);
+		
 		this.getChildren().add(separator);
 		this.setSpacing(10);
 	}
 	
 	public void show(Entry[] data, Integer span, String name) {
-		chart.setTitle(name);
+		
 		Series<String, Number> series = new XYChart.Series<String, Number>();
 		series.setName("Prices of: " + name);
-		if (data.length <= span) {		
-			for (int i = 0; i < data.length; i++) {
-				series.getData().add(new XYChart.Data<>(data[i].getDate(), data[i].getClose().setScale(2, RoundingMode.HALF_UP)));
-			}
-		} else {
-			int start = data.length - span - 1;
-			for (int i = start; i < data.length; i++) {
-				series.getData().add(new XYChart.Data<>(data[i].getDate(), data[i].getClose().setScale(2, RoundingMode.HALF_UP)));
-			}
+		
+		int start = 0;
+		BigDecimal min = data[data.length - 1].getClose().setScale(2, RoundingMode.HALF_UP);
+		BigDecimal max = data[data.length - 1].getClose().setScale(2, RoundingMode.HALF_UP);
+		if (data.length > span) {		
+			start = data.length - span - 1;
 		}
-		chart.getData().remove(0);
+		for (int i = start; i < data.length; i++) {
+			BigDecimal price = data[i].getClose().setScale(2, RoundingMode.HALF_UP);
+			series.getData().add(new XYChart.Data<>(data[i].getDate(), price));
+			if (price.compareTo(min) == -1) {
+				min = price;
+			}
+			
+			if (price.compareTo(max) == 1) {
+				max = price;
+			}
+			
+		}
+		
+		min = min.subtract(min.divide(new BigDecimal(100))).setScale(0, RoundingMode.HALF_UP);
+		max = max.setScale(0, RoundingMode.HALF_UP);
+		
+		xAxis = new CategoryAxis();
+		yAxis = new NumberAxis();
+		yAxis.setAutoRanging(false);
+		yAxis.setUpperBound(max.doubleValue());
+		yAxis.setLowerBound(min.doubleValue());
+		
+		xAxis.setLabel("Date");
+		yAxis.setLabel("Price");
+		
+		chart = new LineChart<String, Number>(xAxis, yAxis);
+		
+		chart.setTitle(name);
 		chart.getData().add(series);
 		chart.setCreateSymbols(false);
+		
+		this.getChildren().clear();
+		this.getChildren().addAll(chart, separator);
 	}
 
 }
