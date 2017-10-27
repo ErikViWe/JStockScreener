d3d9adea764f9914a84e4cab5bd080853a8852b6
ErikViWe <erik.weinstock@googlemail.com>
2017-10-27 13:49:53 +0200

    Removed error and overloaded method.

--- src/average/ExponentialMovingAverage.java
+++ src/average/ExponentialMovingAverage.java
@@ -9,11 +9,28 @@ public class ExponentialMovingAverage {
 	
 	public static BigDecimal[] getExponentialAverage(Entry[] data, int n) {
 		BigDecimal[] arr = new BigDecimal[data.length];
-		BigDecimal alpha = new BigDecimal(2 / (n + 1));
+		BigDecimal alpha = new BigDecimal(2 / (double) (n + 1));
 		BigDecimal alphaInv = new BigDecimal(1).subtract(alpha);
+		System.out.println(alpha.doubleValue());
+		System.out.println(alphaInv.doubleValue());
 		arr[0] = data[0].getClose().setScale(2, RoundingMode.HALF_UP);
 		for (int i = 1; i < data.length; i++) {
 			arr[i] = data[i].getClose().multiply(alpha).add(arr[i - 1].multiply(alphaInv));
+			System.out.println(n + ": " + arr[i].doubleValue());
+		}
+		return arr;
+	}
+	
+	public static BigDecimal[] getExponentialAverage(BigDecimal[] data, int n) {
+		BigDecimal[] arr = new BigDecimal[data.length];
+		BigDecimal alpha = new BigDecimal(2 / (double) (n + 1));
+		BigDecimal alphaInv = new BigDecimal(1).subtract(alpha);
+		System.out.println(alpha.doubleValue());
+		System.out.println(alphaInv.doubleValue());
+		arr[0] = data[0];
+		for (int i = 1; i < data.length; i++) {
+			arr[i] = data[i].multiply(alpha).add(arr[i - 1].multiply(alphaInv));
+			System.out.println(n + ": " + arr[i].doubleValue());
 		}
 		return arr;
 	}
