bee803b2109de3ece0f19b3a2b56eaf483287f08
ErikViWe <erik.weinstock@googlemail.com>
2017-10-30 13:29:25 +0100

    Removed console output from EMA.

--- src/average/ExponentialMovingAverage.java
+++ src/average/ExponentialMovingAverage.java
@@ -11,12 +11,9 @@ public class ExponentialMovingAverage {
 		BigDecimal[] arr = new BigDecimal[data.length];
 		BigDecimal alpha = new BigDecimal(2 / (double) (n + 1));
 		BigDecimal alphaInv = new BigDecimal(1).subtract(alpha);
-		System.out.println(alpha.doubleValue());
-		System.out.println(alphaInv.doubleValue());
 		arr[0] = data[0].getClose().setScale(2, RoundingMode.HALF_UP);
 		for (int i = 1; i < data.length; i++) {
 			arr[i] = data[i].getClose().multiply(alpha).add(arr[i - 1].multiply(alphaInv));
-			System.out.println(n + ": " + arr[i].doubleValue());
 		}
 		return arr;
 	}
@@ -25,12 +22,9 @@ public class ExponentialMovingAverage {
 		BigDecimal[] arr = new BigDecimal[data.length];
 		BigDecimal alpha = new BigDecimal(2 / (double) (n + 1));
 		BigDecimal alphaInv = new BigDecimal(1).subtract(alpha);
-		System.out.println(alpha.doubleValue());
-		System.out.println(alphaInv.doubleValue());
 		arr[0] = data[0];
 		for (int i = 1; i < data.length; i++) {
 			arr[i] = data[i].multiply(alpha).add(arr[i - 1].multiply(alphaInv));
-			System.out.println(n + ": " + arr[i].doubleValue());
 		}
 		return arr;
 	}
